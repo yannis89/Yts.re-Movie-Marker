a875d6fee74cb42aee81c75393201186d43240a6
yannis89 <p.yannis89@gmail.com>
2014-09-18 12:18:48 +0300

    updated

--- yts-movie-marker/background.js
+++ yts-movie-marker/background.js
@@ -4,8 +4,3 @@ function onRequest(request, sender, sendResponse) {
 }
 chrome.extension.onRequest.addListener(onRequest);
 
-/* <div>Icons made by Freepik from 
- * <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> 
- * is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 
- * title="Creative Commons BY 3.0">CC BY 3.0</a></div>
- */
